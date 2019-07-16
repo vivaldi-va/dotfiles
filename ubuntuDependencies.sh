86122ba14a44bc7f89def5dabd1dc2aaa961570d
Zaccary Price <wildtangomedia@gmail.com>
2019-07-16 22:43:16 +0100

    add docker install script for ubuntu

--- ubuntuDependencies.sh
+++ ubuntuDependencies.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# nvim
 sudo add-apt-repository ppa:neovim-ppa/stable
+
+# weechat
+sudo sh -c 'echo "deb https://weechat.org/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list.d/weechat.list'
+sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 11E9DE8848F2B65222AA75B8D1820DB22A11534E
 
 sudo apt-get update > /dev/null && \
   sudo apt-get install -y \
@@ -8,7 +13,8 @@ sudo apt-get update > /dev/null && \
     neovim \
     vim \
     silversearcher-ag \
-    tmux
+    tmux \
+    weechat
 
 # FD find
 wget https://github.com/sharkdp/fd/releases/download/v7.3.0/fd_7.3.0_amd64.deb -O ~/fd.deb && \
@@ -17,9 +23,3 @@ wget https://github.com/sharkdp/fd/releases/download/v7.3.0/fd_7.3.0_amd64.deb -
 
 # FZF
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
-
-# install weechat
-sudo sh -c 'echo "deb https://weechat.org/ubuntu $(lsb_release -cs) main" >> /etc/apt/sources.list.d/weechat.list'
-sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 11E9DE8848F2B65222AA75B8D1820DB22A11534E
-sudo apt-get update > /dev/null && \
-  sudo apt-get install -y weechat
