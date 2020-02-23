fee3f70424d08151b59bbaf405f2fe9b130e4dc2
Zaccary Price <wildtangomedia@gmail.com>
2020-02-23 17:24:54 +0000

    add gdrive backup script

---
+++ Scripts/gdrivebackup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+[ ! -d "$HOME/Drive" ] && mkdir $HOME/Drive
+/usr/bin/rclone sync \
+  --verbose \
+  --transfers 30 \
+  --checkers 8 \
+  --contimeout 60s \
+  --timeout 300s \
+  --retries 3 \
+  --low-level-retries 10 \
+  --stats 1s \
+  --copy-links \
+  "$HOME/Drive" \
+  "google-drive:Sync"
