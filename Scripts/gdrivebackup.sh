4e286ba5e8bdb70ec4eca0d92e34734330438e10
Zaccary Price <wildtangomedia@gmail.com>
2020-02-23 22:54:19 +0000

    copy from remote drive before syncing
    due to sync making src equal to dest, which will delete all
    remote files if src is fresh, and therefor empty

--- Scripts/gdrivebackup.sh
+++ Scripts/gdrivebackup.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 [ ! -d "$HOME/Drive" ] && mkdir $HOME/Drive
+
+/usr/bin/rclone copy \
+  --verbose \
+  --transfers 30 \
+  --checkers 8 \
+  --contimeout 60s \
+  --timeout 300s \
+  --retries 3 \
+  --low-level-retries 10 \
+  --stats 1s \
+  "google-drive:Sync" \
+  "$HOME/Drive"
+
+
 /usr/bin/rclone sync \
   --verbose \
   --transfers 30 \
