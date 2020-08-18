984c53c22bcb714b35852cd442dc0b027fa5ffe0
Zaccary Price <wildtangomedia@gmail.com>
2020-08-18 13:59:31 +0100

    add slr cam preview using ffplay

--- .oh-my-zsh/custom/slr.zsh
+++ .oh-my-zsh/custom/slr.zsh
@@ -3,3 +3,7 @@ function cam() {
   gphoto2 --stdout --capture-movie | \
     ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 $loopback
 }
+
+function campreview() {
+  gphoto2 --stdout --capture-movie | ffplay -
+}
