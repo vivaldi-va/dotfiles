cba3d7f20b8c5eaa7687401f320e47da21c84d25
Zaccary Price <wildtangomedia@gmail.com>
2020-08-26 10:25:17 +0100

    dont exit out of util, since it closes tmux panes

--- .oh-my-zsh/custom/diff.zsh
+++ .oh-my-zsh/custom/diff.zsh
@@ -1,9 +1,7 @@
 function diff() {
   if [ $# -lt 2 ]; then
-    echo "Usage: \n \
-      $0 file1 file2" >&2
-    exit 1
+    echo "usage: $0 file1 file2" >&2
+  else
+    git diff --no-index $1 $2
   fi
-
-  git diff --no-index $1 $2
 }
