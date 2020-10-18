dbf8cd728a8a2c56599d9767da3fea64f33b5e72
Zaccary Price <wildtangomedia@gmail.com>
2020-10-18 01:18:01 +0100

    setup loopback if it doesn't exist when running cam()

--- .oh-my-zsh/custom/slr.zsh
+++ .oh-my-zsh/custom/slr.zsh
@@ -1,5 +1,15 @@
+function getvirtcam() {
+  v4l2-ctl --list-devices | awk '/VirtualCam/{getline; print $1}'
+}
+
 function cam() {
-  loopback=/dev/video4
+  loopback=$(getvirtcam)
+  if [[ -z $loopback ]]; then
+    echo "Loopback device not found, creating one"
+    sudo modprobe v4l2loopback devices=1 max_buffers=2 exclusive_caps=1 card_label="VirtualCam"
+    sleep 5
+  fi
+
   gphoto2 --stdout --capture-movie | \
     ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 $loopback
 }
