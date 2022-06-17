a8ad09595035be0ec50887aef4b3156939c5c0cd
Zaccary Price <wildtangomedia@gmail.com>
2022-06-17 12:05:06 +0100

    move some deps to display only

--- ubuntuDependencies.sh
+++ ubuntuDependencies.sh
@@ -6,21 +6,29 @@ sudo apt-get update > /dev/null && \
     vim \
     silversearcher-ag \
     tmux \
-    ranger \
-    mpv \
-    inotify-tools \
-    konsole
+    ranger
 
 ./ubuntudeps/neovim.sh
 ./ubuntudeps/fd.sh
 ./ubuntudeps/ctag.sh
+./ubuntudeps/ghcli.sh
+./ubuntudeps/sile.sh
+./ubuntudeps/weechat.sh
 
+# install xfce theme
 if [[ $DISPLAY ]]; then
   ./ubuntudeps/qogir-theme.sh
 fi
 
+# install desktop-only stuff
+if [[ $DISPLAY ]]; then
+  ./ubuntudeps/rofi.sh
+  ./ubuntudeps/glow.sh
 
-./ubuntudeps/ghcli.sh
-./ubuntudeps/glow.sh
-./ubuntudeps/sile.sh
-./ubuntudeps/weechat.sh
+  sudo apt-get update > /dev/null && \
+    sudo apt-get install -y -qq \
+    mpv \
+    konsole \
+    inotify-tools \
+    gphoto2
+fi
