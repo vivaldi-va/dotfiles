b2e4362c668b3faf24af9432eff6abfd2c9a9ab3
Zaccary Price <wildtangomedia@gmail.com>
2022-07-08 10:12:53 +0100

    add ubuntu chrome install

---
+++ ubuntudeps/chrome.sh
@@ -0,0 +1,7 @@
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+sudo apt update > /dev/null && \
+  sudo apt install -y -qq \
+    google-chrome-stable \
+    google-chrome-unstable
