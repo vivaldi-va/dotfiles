80dde0076a38691a5379ee1544b1c16965870297
Zaccary Price <wildtangomedia@gmail.com>
2024-10-25 22:46:03 +0100

    snap-refresh: dont quit out when kill can't find a process

--- home/.oh-my-zsh/custom/snap-refresh.zsh
+++ home/.oh-my-zsh/custom/snap-refresh.zsh
@@ -1,3 +1,4 @@
 function snaprefresh() {
-  sudo snap refresh --list | awk '{if(NR>1){print $1}}' | xargs -n1 killall && sudo snap refresh
+  sudo snap refresh --list | awk '{if(NR>1){print $1}}' | xargs -n1 killall 2> /dev/null
+  sudo snap refresh
 }
