4e22570b8a0d2140f25439389e9420d8d782e0c7
Zaccary Price <wildtangomedia@gmail.com>
2025-01-30 14:41:51 +0000

    dont `exit` when genpass fails

--- home/.oh-my-zsh/custom/genpass.zsh
+++ home/.oh-my-zsh/custom/genpass.zsh
@@ -2,8 +2,7 @@ function genpass() {
   if [  $# -lt 1 ]; then
     echo "Usage: \n \
       $0 <charlen>" >&2
-    exit
+  else
+    tr -dc A-Za-z0-9 </dev/urandom | head -c $1; echo
   fi
-
-  tr -dc A-Za-z0-9 </dev/urandom | head -c $1; echo
 }
