23070ef513d76316a2d35d9182d3922a72b69d93
Zaccary Price <wildtangomedia@gmail.com>
2026-08-31 12:32:36 +0100

    add mkgp docs, missing label check

--- home/.oh-my-zsh/custom/mkctl.zsh
+++ home/.oh-my-zsh/custom/mkctl.zsh
@@ -12,12 +12,19 @@ function mkgp() {
   local namespace="${namespace[-1]:-\"default\"}"
   local resource="${resource[-1]:-pod}"
   local label=""
-  local labels=""
+  local describe=(
+    "microk8s kubectl helper to get a \"describe\" for a defined resource in a namespace"
+  )
   local usage=(
-    #"microk8s kubectl helper to get a \"describe\" for a defined resource in a namespace\n"
-    "Usage:"
-    "mkgp [-h|--help]"
-    "mkgp [-r|--resource pod|deploy|svc] [-n|--namespace default] [-l|--label foo=bar]"
+    "Usage (defaults shown):"
+    "\tmkgp [-h|--help]"
+    "\tmkgp [-r|--resource pod] [-n|--namespace default] -l|--label"
+  )
+  local man=(
+    "Options:"
+    "\t-l --label\n\t\t(required) label to select defined resource"
+    "\t-n --namespace\n\t\tresource namespace"
+    "\t-r --resource\n\t\tresource type, e.g. pod,svc,deploy,etc."
   )
 
 
@@ -32,8 +39,17 @@ function mkgp() {
   fi
 
   if (( $#help )); then
+    print "${describe}"
+    print ""
     print -l $usage
+    print -l $man
     return 0
+  fi
+
+  if (( !$#label )); then
+    echo >&2 "label is required"
+    print -l $usage
+    return 1
   fi
 
   local resource_result=$(kubectl -n ${namespace} get ${resource} -l ${opts[-l]} --no-headers -o name|cut -d/ -f2|head -1)
@@ -42,5 +58,6 @@ function mkgp() {
     echo >&2 "no resource of type '${resource}' found in namespace: '${namespace}' with label: '${opts[-l]}'"
     return 1
   fi
+
   microk8s kubectl -n "${namespace}" describe ${resource} ${resource_result}
 }
