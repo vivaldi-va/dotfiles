4e8d7469128c61764398df28ea5a8f2d3d737cb5
Zaccary Price <wildtangomedia@gmail.com>
2026-09-17 12:59:47 +0100

    add wez window title formatting, set titles for tmux

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -19,4 +19,20 @@ config.command_palette_font_size = 12
 
 config.color_scheme = 'GruvboxDark'
 
+wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
+  local title = pane.foreground_process_name or pane.title or ''
+
+  local mux_pane = mux.get_pane(pane.pane_id)
+  if mux_pane then
+    local ok, info = pcall(function()
+      return mux_pane:get_foreground_process_info()
+    end)
+    if ok and info and info.argv and #info.argv > 0 then
+      title = table.concat(info.argv, ' ')
+    end
+  end
+
+  return title
+end)
+
 return config
